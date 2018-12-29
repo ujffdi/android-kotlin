afdd6d16ca45785d19ea1ca460152a48d2779bdd
ujffdi <1009956987@qq.com>
2018-12-29 14:52:28 +0800

    1)kotlin配置

---
+++ app/src/main/java/com/jwsr/eyepetizer/App.kt
@@ -0,0 +1,14 @@
+package com.jwsr.eyepetizer
+
+import com.jwsr.baselib.BaseApp
+
+/**
+ * <pre>
+ *     author : Tongsr
+ *     time   : 2018/12/28
+ *     desc   :
+ * </pre>
+ */
+class App: BaseApp() {
+
+}
---
+++ baseLib/src/main/java/com/jwsr/baselib/BaseApp.kt
@@ -0,0 +1,19 @@
+package com.jwsr.baselib
+
+import android.app.Application
+
+/**
+ * <pre>
+ *     author : Tongsr
+ *     time   : 2018/12/28
+ *     desc   : Application
+ * </pre>
+ */
+abstract class BaseApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
